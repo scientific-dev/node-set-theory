45cf99c19818cf464017cd0c7f3d37f2f512f7a5
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-10-22 12:25:03 +0530

    Using getters in v0.0.3

--- src/Class.ts
+++ src/Class.ts
@@ -4,23 +4,17 @@ import { n, P, proper, removeDuplicates, parse } from './Util';
 export class Set{
 
     set: any[]
-    size: number;
-    length: number;
-    proper: any[];
-    null: boolean;
-    sizeType: 'empty' | 'singleton' | 'many';
-    subsets: any[];
-    properSubsets: any[];
 
     constructor(set: any[]){
         this.set = parse(set);
-        this.size = this.set.length;
-        this.length = this.size;
-        this.proper = proper(this.set);
-        this.null = Methods.isNull(this.set);
-        this.sizeType = Methods.sizeType(this.set);
-        this.subsets = Methods.subsets(this.set);
-        this.properSubsets = Methods.properSubsets(this.set);
     };
+
+    get size(): number { return this.set.length };
+    get length(): number { return this.set.length };
+    get proper(): any[] { return proper(this.set) };
+    get null(): boolean { return Methods.isNull(this.set) };
+    get sizeType(): 'empty' | 'singleton' | 'many' { return Methods.sizeType(this.set) };
+    get subsets(): any[] { return Methods.subsets(this.set) };
+    get properSubsets(): any[] { return Methods.properSubsets(this.set) };
 
 };
